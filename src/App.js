3b7089eb112b435adcacad9cc627a1cfe85a9868
Gabriel Tijon da Silva <75736869+TijonSilva@users.noreply.github.com>
2021-08-18 22:01:29 -0300

    criando uma propriedade

--- src/App.js
+++ src/App.js
@@ -3,7 +3,23 @@ import React from 'react';
 import Header from './components/Header'
 
 function App( ){
- return <Header/> ;
+ return (
+ <>
+ <Header title= "HomePage">
+  <ul>
+   <li>Homepage</li>
+   <li>Project</li>
+  </ul>
+ </Header>
+ <Header title= "projects">
+  <ul>
+   <li>Homepage</li>
+   <li>Projects</li>
+   <li>Login</li>
+   </ul>
+   </Header>
+ </>
+ )
 }
 
 export default App;
