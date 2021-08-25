52f0b3dd70d6356d0483ddca7be1282f53179dc6
Gabriel Tijon da Silva <75736869+TijonSilva@users.noreply.github.com>
2021-08-25 14:24:45 -0300

    utilizando css com react

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React, { useState } from 'react';
-import Header from './components/Header'
+
+import './App.css';
+import background1Image from './assets/background1.jpg';
+
+import Header from './components/Header';
 
 function App() {
   const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
@@ -11,6 +15,8 @@ function App() {
   return (
     <>
       <Header title="Projects" />
+
+      <img width={300} src={background1Image} />
 
       <ul>
         {projects.map(project => <li key={project} > {project}</li>)}
--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,12 @@ module.exports = {
      { loader: 'style-loader' },
      { loader: 'css-loader' }
     ]
+   },
+   {
+    test: /.*\.(gif|png|jpe?g)$/i,
+    use: {
+     loader: 'file-loader'
+    }
    }
   ]
  }
