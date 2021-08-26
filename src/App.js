9dff96a899de48679175a07fdfc4d5f8868504e8
Gabriel Tijon da Silva <75736869+TijonSilva@users.noreply.github.com>
2021-08-26 20:33:59 -0300

    likando api

--- src/App.js
+++ src/App.js
@@ -5,14 +5,13 @@ import './App.css';
 
 
 import Header from './components/Header';
-import { response } from 'express';
 
 function App() {
-  const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front-end web']);
+  const [projects, setProjects] = useState([]);
 
   useEffect(() => {
     api.get('projects').then(response => {
-      console.log(response);
+      setProjects(response.data)
     })
   }, []);
 
@@ -25,7 +24,7 @@ function App() {
       <Header title="Projects" />
 
       <ul>
-        {projects.map(project => <li key={project} > {project}</li>)}
+        {projects.map(project => <li key={project.id} > {title}</li>)}
       </ul>
 
       <button type="butoon" onClick={handleAddProject}>Adicionar Projeto</button>
